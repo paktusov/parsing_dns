cf1bb6422125fef5dc588b5e17c28010172b92b3
Arkadii Paktusov <paktusov@gmail.com>
2022-01-10 02:24:27 +0500

    new tree

---
+++ crawler/product/items.py
@@ -0,0 +1,18 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+#from scrapy.item import Item, Field
+import scrapy
+from dataclasses import dataclass
+
+
+class ProductItem(scrapy.Item):
+    _id = scrapy.Field()
+    name = scrapy.Field()
+    description = scrapy.Field()
+    full_price = scrapy.Field()
+    history_price = scrapy.Field()
+    link = scrapy.Field()
+    image = scrapy.Field()
+
---
+++ crawler/product/pipelines.py
@@ -0,0 +1,56 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+
+
+#class TutorialPipeline:
+#    def process_item(self, item, spider):
+#        return item
+
+import logging
+import pymongo
+
+class MongoPipeline(object):
+
+    collection_name = 'dns_goods'
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        ## pull in information from settings.py
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE')
+        )
+
+    def open_spider(self, spider):
+        ## initializing spider
+        ## opening db connection
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        ## clean up when spider is closed
+        self.client.close()
+
+    def process_item(self, item, spider):
+        exist = self.db[self.collection_name].find_one({"_id": dict(item)["_id"]})
+        if not exist:
+            self.db[self.collection_name].insert_one(ItemAdapter(item).asdict())
+            logging.debug("Item added to MongoDB")
+        else:
+            if exist["history_price"][-1][0] != item["history_price"][-1][0]:
+                exist["history_price"].append(item["history_price"][-1])
+                self.db[self.collection_name].find_one_and_replace({"_id": dict(item)["id"]}, exist)
+                logging.debug("Item update to MongoDB")
+            else:
+                logging.debug("Item duplicates")
+        return item
