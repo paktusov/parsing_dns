ea6ab58d998b5b0715032378a96317f9079b337e
Arkadii Paktusov <paktusov@gmail.com>
2022-01-16 00:57:10 +0500

    remark

--- http/app.py
+++ http/app.py
@@ -13,10 +13,12 @@ app.debug = True
 
 @app.route('/')
 def index():
+    title = 'Markdown'
+    header = 'Markdown'
     products = list(db[collection_name].find().sort("last_update", pymongo.DESCENDING))
     for i in range(len(products)):
         products[i]['last_update'] = dt.datetime.fromisoformat(products[i]['last_update']).strftime("%Y.%m.%d %H:%M")
-    return render_template('index.html', products=products)
+    return render_template('index.html', products=products, title=title, header=header)
 
 
 if __name__ == "__main__":
