6d402ca2c2725f204b3db93d7562a3cdc398b82c
Arkadii Paktusov <paktusov@gmail.com>
2022-01-22 14:59:24 +0500

    add category

--- crawler/product/spiders/product_spider.py
+++ crawler/product/spiders/product_spider.py
@@ -22,29 +22,32 @@ class ProductSpider(scrapy.Spider):
         yield SeleniumRequest(url=url, callback=self.parse_result, cookies={'city_path': 'chelyabinsk'})
 
     def parse_result(self, response):
-        for product in response.css('div.catalog-product'):
+        for product in response.css('div.markdown-page__group-title, div.catalog-product'):
+            if product.css('div.markdown-page__group-title'):
+                category = product.css('div.markdown-page__group-title::text').get()
+                print(category)
+            else:
+                name, *description = product.css('a.catalog-product__name span::text').getall()
+                description = description[0].strip("[]") if description else None
+                link = response.urljoin(product.css('a.catalog-product__name::attr(href)').get())
+                now = dt.datetime.now().isoformat()
 
-            name, *description = product.css('a.catalog-product__name span::text').getall()
-            description = description[0].strip("[]") if description else None
-            link = response.urljoin(product.css('a.catalog-product__name::attr(href)').get())
-            now = dt.datetime.now().isoformat()
-
-            yield ProductItem(
-                _id=link.strip("/").split("/")[-1],
-                name=name,
-                description=description,
-                full_price=parse_price(product.css('div.catalog-product__price-old::text').get()),
-                history_price=[(parse_price(product.css('div.catalog-product__price-actual::text').get()), now)],
-                link=link,
-                image=product.css('div.catalog-product__image img::attr(data-src)').get(),
-                last_update=now,
-                last_seen=now,
-                removed=False
-            )
+                yield ProductItem(
+                    _id=link.strip("/").split("/")[-1],
+                    name=name,
+                    category=category,
+                    description=description,
+                    full_price=parse_price(product.css('div.catalog-product__price-old::text').get()),
+                    history_price=[(parse_price(product.css('div.catalog-product__price-actual::text').get()), now)],
+                    link=link,
+                    image=product.css('div.catalog-product__image img::attr(data-src)').get(),
+                    last_update=now,
+                    last_seen=now,
+                    removed=False
+                    )
 
         next_page = response.css('button.pagination-widget__show-more-btn span::text').get()
         if next_page is not None:
-
             self.i += 1
             next_page = f'https://www.dns-shop.ru/catalog/markdown/?p={self.i}'
             yield SeleniumRequest(url=next_page, callback=self.parse_result, cookies={'city_path': 'chelyabinsk'})
