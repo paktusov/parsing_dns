eb8a748bc9d8306b7d9ae6e1f4f29d8d63e5e25e
Arkadii Paktusov <paktusov@gmail.com>
2022-02-16 22:45:16 +0500

    DNS-4 first step

---
+++ app/crawler/tasks.py
@@ -0,0 +1,24 @@
+from celery import Celery
+from celery.schedules import crontab
+import update_notification
+
+app = Celery('tasks', broker='redis://localhost')
+app.conf.timezone = 'Asia/Yekaterinburg'
+
+app.conf.beat_schedule = {
+    'parsing_chelyabinsk_every_20_minutes': {
+        'task': 'tasks.start_parsing',
+        'schedule': crontab(minute='*/1'),
+        'args': ('chelyabinsk')
+    },
+    'parsing_ekaterinburg_once_a_day': {
+        'task': 'tasks.start_parsing',
+        'schedule': crontab(minute=0, hour=2),
+        'args': ('ekaterinburg')
+    }
+}
+
+@app.task
+def start_parsing(city):
+    update_notifications(city)
+
