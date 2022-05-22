f9a6477289def2afbbdb5029f9fa1ab1e5a88e03
Arkadii Paktusov <paktusov@gmail.com>
2022-05-22 20:45:43 +0500

    transfer db in local docker

--- app/mongo.py
+++ app/mongo.py
@@ -5,8 +5,6 @@ from config import mongo_config
 def get_db():
     client = pymongo.MongoClient(
         mongo_config.uri,
-        username=mongo_config.username,
-        password=mongo_config.password,
         connect=False
     )
     return client[mongo_config.database]
