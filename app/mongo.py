ac51ebbaefecbcd644d56cda6300759fa96f5087
Arkadii Paktusov <paktusov@gmail.com>
2022-05-25 20:30:47 +0500

    edits

--- app/mongo.py
+++ app/mongo.py
@@ -5,6 +5,8 @@ from config import mongo_config
 def get_db():
     client = pymongo.MongoClient(
         mongo_config.uri,
+        username=mongo_config.username,
+        password=mongo_config.password,
         connect=False
     )
     return client[mongo_config.database]
